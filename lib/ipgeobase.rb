6f98ebf9d2d20f329f3f143673c91bad0a265f4d
theworking <marsthegod@protonmail.com>
2022-02-15 03:00:41 +0300

    14

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -12,15 +12,12 @@ module Ipgeobase
     @base_url = "http://ip-api.com/json/"
     # принимает IP-адрес и возвращает объект метаданных
     def self.lookup(ip)
-      # string = @base_url + ip.to_s
       conn = Faraday.new(@base_url) do |f|
         f.request :json # encode req bodies as JSON
         f.adapter :net_http # Use the Net::HTTP adapter
-        f.headers { 'Accept' => 'text/html' }
       end
       response = conn.get(ip.to_s)
       result = JSON.parse(response.body)
-
       result.each do |k, v|
         instance_variable_set "@#{k}", v
         result.define_singleton_method k do
